6fa67c492a630dc1884082249a049107856c3018
shaishabcoding <shaishabchandrashil@gmail.com>
2025-03-31 15:39:43 +0600

    added delete review routes

--- src/routes/v1.ts
+++ src/routes/v1.ts
@@ -8,6 +8,7 @@ import AdminRoutes from '../app/modules/admin/Admin.routes';
 // import { OrderRoutes } from '../app/modules/order/Order.route';
 import { PaymentRoutes } from '../app/modules/payment/Payment.route';
 import { ProductRoutes } from '../app/modules/product/Product.route';
+import ReviewRoutes from '../app/modules/review/Review.route';
 // import { CartRoutes } from '../app/modules/cart/Cart.route';
 
 const routes: TRoute[] = [
@@ -28,6 +29,11 @@ const routes: TRoute[] = [
   {
     path: '/products',
     route: ProductRoutes.user,
+  },
+  {
+    path: '/reviews',
+    middlewares: [auth(EUserRole.USER, EUserRole.ADMIN)],
+    route: ReviewRoutes,
   },
   // {
   //   path: '/orders',
