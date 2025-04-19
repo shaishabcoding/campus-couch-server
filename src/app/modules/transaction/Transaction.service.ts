f6800a45910c91551ae77a96e4f493a7dbcef55d
shaishabcoding <shaishabchandrashil@gmail.com>
2025-04-19 15:17:49 +0600

    added transcations

--- src/app/modules/transaction/Transaction.service.ts
+++ src/app/modules/transaction/Transaction.service.ts
@@ -7,15 +7,94 @@ export const TransactionServices = {
   },
 
   async list({ page = 1, limit = 10 }: Record<string, any>) {
-    const transactions = await Transaction.find()
-      .populate('user', 'name avatar email')
-      .skip((page - 1) * limit)
-      .limit(limit);
+    const pipeline: any[] = [
+      {
+        $lookup: {
+          from: 'users',
+          localField: 'user',
+          foreignField: '_id',
+          as: 'user',
+        },
+      },
+      { $unwind: '$user' },
+      {
+        $facet: {
+          paginatedResults: [
+            { $sort: { createdAt: -1 } },
+            { $skip: (page - 1) * limit },
+            { $limit: limit },
+          ],
+          totalCount: [{ $count: 'count' }],
+          totalBuy: [
+            { $match: { transaction_type: 'buy' } },
+            { $group: { _id: null, total: { $sum: '$amount' } } },
+          ],
+          totalSell: [
+            { $match: { transaction_type: 'sell' } },
+            { $group: { _id: null, total: { $sum: '$amount' } } },
+          ],
+          yearlyEarnings: [
+            {
+              $group: {
+                _id: { $month: '$createdAt' },
+                totalBuy: {
+                  $sum: { $cond: [{ $eq: ['$transaction_type', 'buy'] }, '$amount', 0] },
+                },
+                totalSell: {
+                  $sum: { $cond: [{ $eq: ['$transaction_type', 'sell'] }, '$amount', 0] },
+                },
+              },
+            },
+            { $sort: { _id: 1 } },
+          ],
+        },
+      },
+    ];
 
-    const total = await Transaction.countDocuments();
+    const result = (await Transaction.aggregate(pipeline))[0];
+
+    const transactions = result?.paginatedResults || [];
+    const total = result?.totalCount[0]?.count || 0;
+    const totalBuy = result?.totalBuy[0]?.total || 0;
+    const totalSell = result?.totalSell[0]?.total || 0;
+    const totalEarnings = totalSell - totalBuy;
+
+    const months = [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec',
+    ];
+
+    const yearlyEarnings = months.reduce((acc, month, index) => {
+      const data = result[0]?.yearlyEarnings.find(
+        (e: any) => e._id === index + 1
+      ) || {
+        totalBuy: 0,
+        totalSell: 0,
+      };
+
+      const totalEarningsForMonth = data.totalSell - data.totalBuy;
+
+      (acc as any)[month] = {
+        buys: data.totalBuy.toFixed(2),
+        sells: data.totalSell.toFixed(2),
+        earnings: totalEarningsForMonth.toFixed(2),
+      };
+
+      return acc;
+    }, {});
+
 
     return {
-      transactions,
       meta: {
         pagination: {
           page,
@@ -23,7 +102,14 @@ export const TransactionServices = {
           total,
           totalPages: Math.ceil(total / limit),
         },
-      }
+        total: {
+          buys: totalBuy.toFixed(2),
+          sells: totalSell.toFixed(2),
+          earnings: totalEarnings.toFixed(2),
+        },
+        yearlyEarnings,
+      },
+      transactions,
     };
   },
 
