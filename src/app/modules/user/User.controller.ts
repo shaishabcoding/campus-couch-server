1d30b0cc7afe94d246e3e61988c71f5a4be00f3d
shaishabcoding <shaishabchandrashil@gmail.com>
2025-05-12 20:11:47 +0600

    dont do img require

--- src/app/modules/user/User.controller.ts
+++ src/app/modules/user/User.controller.ts
@@ -4,7 +4,7 @@ import serveResponse from '../../../util/server/serveResponse';
 
 export const UserControllers = {
   create: catchAsync(async ({ body }, res) => {
-    body.avatar = body.images[0];
+    body.avatar = body.images?.[0];
 
     await UserServices.create(body);
 
