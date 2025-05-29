14f91ac2267eabbdcd88175afd20e9b913ce4123
shaishabcoding <shaishabchandrashil@gmail.com>
2025-05-29 15:17:41 +0600

    feat: implement image upload to MongoDB GridFS
    This commit introduces a new image upload system that stores images directly in MongoDB GridFS instead of the local file system. This provides benefits like scalability and easier management.
    
    The changes include:
    
    - Adding `multer-gridfs-storage` to the dependencies.
    - Configuring `multer-gridfs-storage` to handle file uploads and store them in GridFS.
    - Adding .env to .gitignore.
    - Implementing middleware for uploading and retrieving images from GridFS.
    - Adding image delete functionality.

--- src/app/middlewares/imageUploader.ts
+++ src/app/middlewares/imageUploader.ts
@@ -1,70 +1,91 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable no-undef, no-unused-vars */
-import path from 'path';
-import multer, { FileFilterCallback } from 'multer';
+import type { Express } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import { GridFsStorage } from 'multer-gridfs-storage';
+import multer, { FileFilterCallback } from 'multer';
 import ServerError from '../../errors/ServerError';
-import { createDir } from '../../util/file/createDir';
 import catchAsync from '../../util/server/catchAsync';
+import config from '../../config';
+import mongoose from 'mongoose';
+import { GridFSBucket } from 'mongodb';
 
 /**
- * @description Multer middleware to handle image uploads with optional resizing.
+ * @description Multer middleware to handle image uploads to MongoDB GridFS
  */
-const imageUploader = () => {
-  const uploadDir = path.join(process.cwd(), 'uploads', 'images');
-  const resizedDir = path.join(uploadDir, 'resized');
+const imageUploader = () =>
+  catchAsync(async (req, res, next) => {
+    await new Promise<void>((resolve, reject) =>
+      upload(req, res, err => (err ? reject(err) : resolve())),
+    );
 
-  createDir(uploadDir, resizedDir);
+    const files = req.files as { images?: Express.Multer.File[] };
+    if (files?.images?.length)
+      req.body.images = files.images.map(
+        ({ filename }) => `/images/${filename}`,
+      );
 
-  const storage = multer.diskStorage({
-    destination: (_req, _file, cb) => cb(null, uploadDir),
-    filename: (_, file, cb) => {
-      const fileExt = path.extname(file.originalname);
-      const fileName =
-        file.originalname
-          .replace(fileExt, '')
-          .toLowerCase()
-          .split(' ')
-          .join('-') +
-        '-' +
-        Date.now();
-      cb(null, fileName + fileExt);
-    },
+    next();
   });
-
-  const fileFilter = (_req: any, file: any, cb: FileFilterCallback) =>
-    file.mimetype.startsWith('image/')
-      ? cb(null, true)
-      : cb(
-          new ServerError(
-            StatusCodes.BAD_REQUEST,
-            'Only image files are allowed',
-          ),
-        );
-
-  const upload = multer({
-    storage,
-    fileFilter,
-  }).fields([{ name: 'images', maxCount: 20 }]);
-
-  return catchAsync((req, res, next) => {
-    upload(req, res, async err => {
-      if (err) next(err);
-
-      const uploadedImages = req.files as { images?: Express.Multer.File[] };
-
-      if (!uploadedImages?.images?.length) return next();
-
-      const resizedImages: string[] = [];
-
-      for (const file of uploadedImages.images) {
-        resizedImages.push(`/images/${file.filename}`);
-      }
-
-      req.body.images = resizedImages;
-      next();
-    });
-  });
-};
 
 export default imageUploader;
+
+/**
+ * @description Retrieves an image from MongoDB GridFS
+ */
+export const imageRetriever = catchAsync(
+  async (req, res) =>
+    new Promise((resolve, reject) =>
+      bucket
+        .openDownloadStreamByName(req.params.filename)
+        .on('error', () =>
+          reject(new ServerError(StatusCodes.NOT_FOUND, 'Image not found')),
+        )
+        .pipe(res)
+        .on('end', resolve),
+    ),
+);
+
+/**
+ * @description Deletes an image from MongoDB GridFS
+ */
+export const deleteImage = async (filename: string) =>
+  Promise.all(
+    (await bucket.find({ filename }).toArray())?.map(({ _id }) =>
+      bucket.delete(_id),
+    ),
+  );
+
+const storage = new GridFsStorage({
+  url: config.url.database,
+  file: (req, { originalname }) => ({
+    filename: `${originalname
+      .replace(/\..+$/, '')
+      .replace(/[^\w]+/g, '-')
+      .toLowerCase()}-${Date.now()}.png`,
+    bucketName: 'images',
+    metadata: { uploadedBy: req.user?._id, originalName: originalname },
+  }),
+});
+
+const fileFilter = (
+  _: any,
+  file: Express.Multer.File,
+  cb: FileFilterCallback,
+) => {
+  if (
+    /^image\/.+/i.test(file.mimetype) ||
+    /\.(jpe?g|png|gif|webp|avif|svg|bmp|tiff?)$/i.test(file.originalname)
+  )
+    return cb(null, true);
+  cb(
+    new ServerError(
+      StatusCodes.BAD_REQUEST,
+      `${file.originalname} is not a valid image file`,
+    ),
+  );
+};
+
+const upload = multer({ storage, fileFilter }).fields([{ name: 'images' }]);
+
+const bucket = new GridFSBucket(mongoose.connection.db!, {
+  bucketName: 'images',
+});
---
+++ src/app/middlewares/imageUploaderIntoFile.ts
@@ -0,0 +1,70 @@
+// /* eslint-disable @typescript-eslint/no-unused-vars */
+// /* eslint-disable no-undef, no-unused-vars */
+// import path from 'path';
+// import multer, { FileFilterCallback } from 'multer';
+// import { StatusCodes } from 'http-status-codes';
+// import ServerError from '../../errors/ServerError';
+// import { createDir } from '../../util/file/createDir';
+// import catchAsync from '../../util/server/catchAsync';
+
+// /**
+//  * @description Multer middleware to handle image uploads with optional resizing.
+//  */
+// const imageUploader = () => {
+//   const uploadDir = path.join(process.cwd(), 'uploads', 'images');
+//   const resizedDir = path.join(uploadDir, 'resized');
+
+//   createDir(uploadDir, resizedDir);
+
+//   const storage = multer.diskStorage({
+//     destination: (_req, _file, cb) => cb(null, uploadDir),
+//     filename: (_, file, cb) => {
+//       const fileExt = path.extname(file.originalname);
+//       const fileName =
+//         file.originalname
+//           .replace(fileExt, '')
+//           .toLowerCase()
+//           .split(' ')
+//           .join('-') +
+//         '-' +
+//         Date.now();
+//       cb(null, fileName + fileExt);
+//     },
+//   });
+
+//   const fileFilter = (_req: any, file: any, cb: FileFilterCallback) =>
+//     file.mimetype.startsWith('image/')
+//       ? cb(null, true)
+//       : cb(
+//           new ServerError(
+//             StatusCodes.BAD_REQUEST,
+//             'Only image files are allowed',
+//           ),
+//         );
+
+//   const upload = multer({
+//     storage,
+//     fileFilter,
+//   }).fields([{ name: 'images', maxCount: 20 }]);
+
+//   return catchAsync((req, res, next) => {
+//     upload(req, res, async err => {
+//       if (err) next(err);
+
+//       const uploadedImages = req.files as { images?: Express.Multer.File[] };
+
+//       if (!uploadedImages?.images?.length) return next();
+
+//       const resizedImages: string[] = [];
+
+//       for (const file of uploadedImages.images) {
+//         resizedImages.push(`/images/${file.filename}`);
+//       }
+
+//       req.body.images = resizedImages;
+//       next();
+//     });
+//   });
+// };
+
+// export default imageUploader;
